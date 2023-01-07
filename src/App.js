eda46dffa90e94880758830961c6e970fba457ca
Ivan Amoshyi <amoshyiivan@gmail.com>
2023-01-07 19:44:34 +0200

    step 2

---
+++ src/App.js
@@ -0,0 +1,79 @@
+import { nanoid } from 'nanoid';
+import { useState } from 'react';
+import {TextField, Button} from '@mui/material';
+import './App.css';
+
+
+function App() {
+  const [contacts, setContacts] = useState([]);
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+  const handleChange = (e) => {
+    switch (e.target.name) {
+      case 'name':
+        setName(e.target.value);
+        break;
+      case 'number':
+        setNumber(e.target.value);
+        break;
+      default:
+        break;
+    }
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const contact = {
+      id: nanoid(),
+      name,
+      number
+    }
+    setContacts(prevState => [...prevState, contact]);
+    setName('');
+    setNumber('');
+  }
+
+  return (
+    <div className="App">
+      <form onSubmit={handleSubmit} autoComplete="off">
+        <TextField
+          name="name"
+          id="outlined-basic"
+          label="Name"
+          variant="outlined"
+          size="small"
+          sx={{ marginBottom: '20px' }}
+          value={name}
+          onChange={handleChange}
+        />
+        <br />
+        <TextField
+          name="number"
+          id="outlined-basic"
+          label="Number"
+          variant="outlined"
+          size="small"
+          value={number}
+          onChange={handleChange}
+        />
+        <br />
+        <Button type="submit" sx={{ marginTop: '20px' }} variant="outlined">
+          Add contact
+        </Button>
+      </form>
+      {contacts && (
+        <ul>
+          {contacts.map(({id, name, number}) => (
+            <li key={id}>
+              <span>{name}</span>
+              <span>{number}</span>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
+
+export default App;
