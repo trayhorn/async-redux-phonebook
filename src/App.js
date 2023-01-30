d9360bce6efb6c753c5c6a979399ad1d863b432b
Ivan Amoshyi <amoshyiivan@gmail.com>
2023-01-30 20:36:16 +0200

    structurized by folders, login in APP

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
-import Form from './components/Form';
-import ContactList from './components/ContactList';
-import Filter from './components/Filter';
-import './App.css';
 import { useState } from 'react';
+import { useGetAllContactsQuery } from 'redux/PhonebookSlice';
+import ContactList from './components/ContactList';
+import Filter from './components/Filter/Filter';
+import Form from './components/Form/Form';
+import './App.css';
 
 
 function App() {
   const [filter, setFilter] = useState('');
+  const { data: contacts } = useGetAllContactsQuery();
+
   const getFilterQuery = (query) => {
     setFilter(query);
   }
+
+  const getVisibleContacts = () => {
+    const nomalizedFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(nomalizedFilter),
+    );
+  };
+
+  if (!contacts) {
+    return;
+  }
+
+  const visibleContacts = getVisibleContacts();
 
   return (
     <div className="App">
@@ -17,7 +33,7 @@ function App() {
       <Form />
       <h2>Contacts</h2>
       <Filter onFilterChange={getFilterQuery} value={filter} />
-      <ContactList filterQuery={filter} />
+      <ContactList visibleContacts={visibleContacts} filterQuery={filter} />
     </div>
   );
 }
--- src/components/ContactList/Contact/Contact.js
+++ src/components/ContactList/Contact/Contact.js
@@ -1,14 +1,14 @@
 import { IconButton } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
 import { useDeleteContactMutation } from 'redux/PhonebookSlice';
-
+import s from '../../ContactList/ContactList.module.css';
 
 export default function Contact({ id, name, phone }) {
   const [deleteContact, { isLoading }] = useDeleteContactMutation();
 
   return (
-    <li className="contactItem">
-      <span className="contactName">{name}</span>
+    <li className={s.contactItem}>
+      <span>{name}</span>
       <span>{phone}</span>
       <IconButton
         aria-label="delete"
